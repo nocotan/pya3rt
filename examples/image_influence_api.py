7373e95c0fe159cc49cacccca01f29817d47b09d
Miura55 <acordion.piano@gmail.com>
2021-12-12 19:05:47 +0900

    APIのアップデートに伴うコードの修正

--- examples/image_influence_api.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import pya3rt
-
-apikey = "{YOUR_API_KEY}"
-client = pya3rt.ImageInfluenceClient(apikey)
-
-print(client.get_upload_url())
-print(client.meat_score("meat1.jpeg", 1))
-
